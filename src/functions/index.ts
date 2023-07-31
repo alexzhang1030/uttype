413d566844df0fed993531ccc30157052d3e1776
alexzhang1030 <1642114555@qq.com>
2023-07-31 21:17:25 +0800

    feat: add entriesFind

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -3,3 +3,12 @@ import type { Recordable } from '../types'
 export function entries<T extends Recordable>(obj: T): [keyof T, T[keyof T]][] {
   return Object.entries(obj) as any
 }
+
+export function entriesFind<
+  T extends Recordable = Recordable,
+>(
+  obj: T,
+  predicate: (item: [keyof T, T[keyof T]]) => boolean,
+) {
+  return entries(obj).find(predicate)
+}
